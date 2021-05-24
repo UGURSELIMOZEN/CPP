cc9d9db6b5e9a03059a5d19d8b20e4eb460771f9
Uğur Selim Özen <72982905+UGURSELIMOZEN@users.noreply.github.com>
2021-05-24 17:48:25 +0300

    Lecture_04_05_06_07_variables_calculator_memory_arithmetic

--- C++ Complete Course/main.cpp
+++ C++ Complete Course/main.cpp
@@ -4,12 +4,35 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
-    cout << "I'am Selim !" << endl;
-    cout << "What's going on ? " ;
-    cout << "I'am saying you !\n" ;
-    cout <<"Do not you answer ?" ;
-    cout << "I'am saying you !\n\n" ;
-    cout <<"Do not you answer ?" ;
+    int var1 = 13 ;
+    int var2 = 18 ;
+    int sum = var1 + var2 ;
+    cout <<sum ;
+
+    int a;
+    int b;
+    int sum1 ;
+
+    cout << "\nEnter a number boss : " ;
+    cin >> a ;
+
+    cout << "\nEnter another number boss : " ;
+    cin >> b ;
+
+    sum1 = a+b ;
+    cout <<"\nThe sum of those numbers is : "<<sum1<<endl;
+
+    int tuana = 23 ;
+    tuana = 54 ;
+    cout <<"Variable is : "<<tuana<<endl ;
+
+    int x = 5+6  ;
+    int y = 34-8 ;
+    int z = 13*3 ;
+    int t = 45/5 ;
+    int f = 59 % 17 ;
+    cout << x <<endl<< y <<endl<< z <<endl<< t <<endl<<f;
+
+
     return 0;
 }
